5d19549b0a19e7003befb3f0da30cfebdd7ac65d
DK\dmsa <dmsa@sitecore.net>
2019-04-04 10:27:45 +0300

    The "ProtectedImageLinkRenderer" class was modified to be compatible with the SItecore 9.0 Initial Release.

--- src/Sitecore.Support.97023/ProtectedImageLinkRenderer.cs
+++ src/Sitecore.Support.97023/ProtectedImageLinkRenderer.cs
@@ -1,17 +1,15 @@
 ﻿namespace Sitecore.Support.Pipelines.RenderField
 {
   using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Web;
   using Sitecore.Pipelines.RenderField;
   using Diagnostics;
   using System.Text;
   using Configuration;
   using Sitecore.StringExtensions;
+  using ProtectedImageLinkRendererOrigin = Sitecore.Pipelines.RenderField.ProtectedImageLinkRenderer;
 
-  public class ProtectedImageLinkRenderer : Sitecore.Pipelines.RenderField.ProtectedImageLinkRenderer
-  {
+  public class ProtectedImageLinkRenderer : ProtectedImageLinkRendererOrigin
+    {
     // Fields
     private readonly char[] quotes = new char[] { '\'', '"' };
 
@@ -19,35 +17,45 @@
     protected new string HashImageReferences(string renderedText)
     {
       Assert.ArgumentNotNull(renderedText, "renderedText");
+
       if (renderedText.IndexOf("<img ", StringComparison.OrdinalIgnoreCase) < 0)
       {
         return renderedText;
       }
+
       if (renderedText.IndexOf(Settings.Media.RequestProtection.HashParameterName + "=", StringComparison.OrdinalIgnoreCase) > 0)
       {
         return renderedText;
       }
+
       int startIndex = 0;
       bool flag = false;
+
       while ((startIndex < renderedText.Length) && !flag)
       {
         int tagStartIndex = renderedText.IndexOf("<img", startIndex, StringComparison.OrdinalIgnoreCase);
+
         if (tagStartIndex < 0)
         {
           break;
         }
+
         flag = base.CheckReferenceForParams(renderedText, tagStartIndex, "img", "src");
         startIndex = renderedText.IndexOf(">", tagStartIndex, StringComparison.OrdinalIgnoreCase) + 1;
       }
+
       if (!flag)
       {
         return renderedText;
       }
+
       startIndex = 0;
       StringBuilder builder = new StringBuilder(renderedText.Length + 0x80);
+
       while (startIndex < renderedText.Length)
       {
         int num3 = renderedText.IndexOf("<img", startIndex, StringComparison.OrdinalIgnoreCase);
+
         if (num3 > -1)
         {
           int num4 = renderedText.IndexOf(">", num3, StringComparison.OrdinalIgnoreCase) + 1;
@@ -62,27 +70,25 @@
           startIndex = 0x7fffffff;
         }
       }
+
       return builder.ToString();
     }
-
 
     public new void Process(RenderFieldArgs args)
     {
       Assert.ArgumentNotNull(args, "args");
+
       try
       {
-        if (this.ShouldProtectRequest(args))
-        {
-          string str = this.HashImageReferences(args.Result.FirstPart);
-          string str2 = this.HashImageReferences(args.Result.LastPart);
-          args.Result.FirstPart = str;
-          args.Result.LastPart = str2;
-        }
+        string str = this.HashImageReferences(args.Result.FirstPart);
+        string str2 = this.HashImageReferences(args.Result.LastPart);
+        args.Result.FirstPart = str;
+        args.Result.LastPart = str2;
       }
       catch (Exception exception)
       {
         object[] parameters = new object[] { args.FieldName };
-        this.Log.Warn("MediaRequestProtection: Could not process {0}".FormatWith(parameters), exception, this);
+        Log.Warn("MediaRequestProtection: Could not process {0}".FormatWith(parameters), exception, this);
       }
     }
 
